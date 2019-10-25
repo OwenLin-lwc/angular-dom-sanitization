5afa45066625c9294c44c4eaaa694f15b9f2f954
OwenLin-lwc <owen4work@gmail.com>
2019-10-25 11:56:36 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  tw = '<iframe width="370" height="215" src="https://www.youtube.com/embed/playlist?list=PL3oW2tjiIxvTUfDOkivqSDxrxfQccwxsN" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-  kr = '<iframe width="370" height="215" src="https://www.youtube.com/embed/watch?v=XsX3ATc3FbA&list=PL4QNnZJr8sRNKjKzArmzTBAlNYBDN2h-J" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-  us = '<iframe width="370" height="215" src="https://www.youtube.com/embed/watch?v=FuXNumBwDOM&list=PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+  twChk = false;
+  krChk = false;
+  usChk = false;
+
+  tw = '<iframe width="720" height="480" src="https://www.youtube.com/embed/playlist?list=PL3oW2tjiIxvTUfDOkivqSDxrxfQccwxsN" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+  kr = '<iframe width="720" height="480" src="https://www.youtube.com/embed/watch?v=XsX3ATc3FbA&list=PL4QNnZJr8sRNKjKzArmzTBAlNYBDN2h-J" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+  us = '<iframe width="720" height="480" src="https://www.youtube.com/embed/watch?v=FuXNumBwDOM&list=PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 
   constructor() {}
 
